e6b976612c78813595f058cd3c9ef9b5e082a0ea
darkohedgehog <zivic.darko79@gmail.com>
2023-07-14 18:25:53 +0200

    Connecting to backend

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://hedgehog-smart-brain.onrender.com/imageurl', {
+      fetch('https://smart-brain-back-end.onrender.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -74,7 +74,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://hedgehog-smart-brain.onrender.com/image', {
+          fetch('https://smart-brain-back-end.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://hedgehog-smart-brain.onrender.com/signin', {
+    fetch('https://smart-brain-back-end.onrender.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
